fc3735dfd936af167010aa15f0f6537f2d048f04
Stefan Widgren <stefan.widgren@gmail.com>
2015-09-20 11:36:34 +0200

    Remove unused methods

--- R/check_arguments.R
+++ R/check_arguments.R
@@ -16,42 +16,6 @@
 ## You should have received a copy of the GNU General Public License
 ## along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-##' Check for null arguments
-##'
-##' Raise an error if any of the arguments are null.
-##' @param ... The argumens to check
-##' @keywords internal
-##' @return invisible(NULL)
-check_null_arg <- function(...) {
-    arg <- list(...)
-    for (i in seq_len(length(arg))) {
-        if (is.null(arg[[i]]))
-            stop(paste0("'",
-                        match.call(expand.dots = FALSE)$'...'[i],
-                        "' is missing"))
-    }
-
-    invisible(NULL)
-}
-
-##' Check numeric arguments
-##'
-##' Raise an error if any of the arguments are non-numeric.
-##' @param ... The argumens to check
-##' @keywords internal
-##' @return invisible(NULL)
-check_numeric_arg <- function(...) {
-    arg <- list(...)
-    for (i in seq_len(length(arg))) {
-        if (!is.numeric(arg[[i]]))
-            stop(paste0("'",
-                        match.call(expand.dots = FALSE)$'...'[i],
-                        "' must be numeric"))
-    }
-
-    invisible(NULL)
-}
-
 ##' Check integer arguments
 ##'
 ##' Raise an error if any of the arguments are non-integer.
