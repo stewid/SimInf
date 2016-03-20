0ed4736ecb825f43e8490ae82bb407e3a5f8368b
Stefan Widgren <stefan.widgren@gmail.com>
2016-03-20 22:42:36 +0100

    Add 'sessionInfo' to test file

--- tests/ldata_sp.R
+++ tests/ldata_sp.R
@@ -1,6 +1,6 @@
 ## SimInf, a framework for stochastic disease spread simulations
-## Copyright (C) 2015  Stefan Engblom
-## Copyright (C) 2015  Stefan Widgren
+## Copyright (C) 2015 - 2016  Stefan Engblom
+## Copyright (C) 2015 - 2016  Stefan Widgren
 ##
 ## This program is free software: you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
@@ -16,6 +16,9 @@
 ## along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 library(SimInf)
+
+## For debugging
+sessionInfo()
 
 ## Define a tolerance
 tol = 1e-8
