daffc8904294ebe84da78f307081f4339bdce4a6
Stefan Widgren <stefan.widgren@gmail.com>
2016-04-09 11:30:38 +0200

    Test prevalence method with a SISe3_sp class

--- tests/measures.R
+++ tests/measures.R
@@ -217,3 +217,83 @@ if (SimInf:::have_openmp()) {
         prevalence(result_omp)[i],
         prevalence(result_omp, by = 2)))
 }
+
+## Check measures with a SISe3_sp model
+u0 <- data.frame(S_1 = rep(10, 10), I_1 = rep( 0, 10),
+                 S_2 = rep(20, 10), I_2 = rep( 0, 10),
+                 S_3 = rep(70, 10), I_3 = rep( 0, 10))
+
+model <- SISe3_sp(u0        = u0,
+                  tspan     = seq_len(1000) - 1,
+                  events    = NULL,
+                  phi       = rep(1, 10),
+                  upsilon_1 = 0.0357,
+                  upsilon_2 = 0.0357,
+                  upsilon_3 = 0.00935,
+                  gamma_1   = 0.1,
+                  gamma_2   = 0.1,
+                  gamma_3   = 0.1,
+                  alpha     = 1.0,
+                  beta_t1   = 0.19,
+                  beta_t2   = 0.085,
+                  beta_t3   = 0.075,
+                  beta_t4   = 0.185,
+                  end_t1    = 91,
+                  end_t2    = 182,
+                  end_t3    = 273,
+                  end_t4    = 365,
+                  coupling = 0,
+                  distance = distance_matrix(1:10, 1:10, 1),
+                  epsilon   = 0)
+
+res <- tools::assertError(susceptible(model))
+stopifnot(length(grep("Please run the model first, the 'U' matrix is empty",
+                      res[[1]]$message)) > 0)
+
+res <- tools::assertError(infected(model))
+stopifnot(length(grep("Please run the model first, the 'U' matrix is empty",
+                      res[[1]]$message)) > 0)
+
+result <- run(model, threads = 1)
+result
+
+stopifnot(identical(length(susceptible(result)), 10000L))
+i <- seq(from = 1, to = 1000, by = 2)
+stopifnot(identical(
+    susceptible(result, age = 1)[, i, drop = FALSE],
+    susceptible(result, age = 1, by = 2)))
+
+stopifnot(identical(length(infected(result)), 10000L))
+stopifnot(identical(
+    infected(result, age = 1)[, i, drop = FALSE],
+    infected(result, age = 1, by = 2)))
+
+stopifnot(identical(length(prevalence(result)), 1000L))
+stopifnot(is.null(dim(prevalence(result))))
+stopifnot(identical(dim(prevalence(result, wnp = TRUE)), c(10L, 1000L)))
+stopifnot(identical(
+    prevalence(result)[i],
+    prevalence(result, by = 2)))
+
+if (SimInf:::have_openmp()) {
+    result_omp <- run(model, threads = 2)
+    result_omp
+
+    stopifnot(identical(length(susceptible(result_omp)), 10000L))
+    i <- seq(from = 1, to = 1000, by = 2)
+    stopifnot(identical(
+        susceptible(result_omp)[, i, drop = FALSE],
+        susceptible(result_omp, by = 2)))
+
+    stopifnot(identical(length(infected(result_omp)), 10000L))
+    stopifnot(identical(
+        infected(result_omp)[, i, drop = FALSE],
+        infected(result_omp, by = 2)))
+
+    stopifnot(identical(length(prevalence(result_omp)), 1000L))
+    stopifnot(is.null(dim(prevalence(result_omp))))
+    stopifnot(identical(dim(prevalence(result_omp, wnp = TRUE)), c(10L, 1000L)))
+    stopifnot(identical(
+        prevalence(result_omp)[i],
+        prevalence(result_omp, by = 2)))
+}
