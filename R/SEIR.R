d921c7cfd6120bdbdef8f3470831d2f4a9cca4f3
Stefan Widgren <stefan.widgren@gmail.com>
2016-12-28 11:58:09 +0100

    Add SEIR model

---
+++ R/SEIR.R
@@ -0,0 +1,227 @@
+## SimInf, a framework for stochastic disease spread simulations
+## Copyright (C) 2015 - 2016  Stefan Widgren
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+##' Class \code{"SEIR"}
+##'
+##' Class to handle the SEIR \code{\link{siminf_model}}.
+##' @include siminf_model.R
+##' @include AllGenerics.R
+##' @export
+setClass("SEIR", contains = c("siminf_model"))
+
+##' Create a SEIR model
+##'
+##' Create a SEIR model to be used by the simulation framework.
+##'
+##'
+##' The argument \code{u0} must be a \code{data.frame} with one row for
+##' each node with the following columns:
+##' \describe{
+##' \item{S}{The number of sucsceptible in each node}
+##' \item{I}{The number of infected in each node}
+##' \item{R}{The number of recovered in each node}
+##' }
+##'
+##' @param u0 A \code{data.frame} with the initial state in each node,
+##'     see details.
+##' @param tspan An increasing sequence of points in time where the
+##'     state of the system is to be returned.
+##' @param events a \code{data.frame} with the scheduled events, see
+##'     \code{\link{siminf_model}}.
+##' @param beta The transmission rate from susceptible to exposed.
+##' @param epsilon The incubation rate from exposed to infected.
+##' @param gamma The recovery rate from infected to recovered.
+##' @return \code{SEIR}
+##' @include check_arguments.R
+##' @export
+SEIR <- function(u0,
+                tspan,
+                events  = NULL,
+                beta    = NULL,
+                epsilon = NULL,
+                gamma   = NULL)
+{
+    compartments <- c("S", "E", "I", "R")
+
+    ## Check arguments.
+
+    ## Check u0
+    if (!is.data.frame(u0))
+        stop("'u0' must be a data.frame")
+    if (!all(compartments %in% names(u0)))
+        stop("Missing columns in u0")
+    u0 <- u0[, compartments]
+
+    ## Check for non-numeric parameters
+    check_gdata_arg(beta, epsilon, gamma)
+
+    ## Arguments seems ok...go on
+
+    E <- Matrix(c(1, 1,
+                  0, 1,
+                  0, 1,
+                  0, 1),
+                nrow   = 4,
+                ncol   = 2,
+                byrow  = TRUE,
+                sparse = TRUE)
+    E <- as(E, "dgCMatrix")
+    colnames(E) <- as.character(1:2)
+    rownames(E) <- compartments
+
+    N <- matrix(integer(0), nrow = 0, ncol = 0)
+
+    G <- Matrix(c(1, 1, 1,
+                  1, 1, 1,
+                  1, 1, 1),
+                nrow = 3,
+                ncol = 3,
+                byrow  = TRUE,
+                sparse = TRUE)
+    G <- as(G, "dgCMatrix")
+    colnames(G) <- as.character(1:3)
+    rownames(G) <- c("S -> E", "E -> I", "I -> R")
+
+    S <- Matrix(c(-1,  0,  0,
+                   1, -1,  0,
+                   0,  1, -1,
+                   0,  0,  1),
+                nrow   = 4,
+                ncol   = 3,
+                byrow  = TRUE,
+                sparse = TRUE)
+    S <- as(S, "dgCMatrix")
+    colnames(S) <- as.character(1:3)
+    rownames(S) <- compartments
+
+    v0 <- matrix(numeric(0), nrow  = 0, ncol = nrow(u0))
+    storage.mode(v0) <- "double"
+
+    ldata <- matrix(numeric(0), nrow = 0, ncol = nrow(u0))
+    storage.mode(ldata) <- "double"
+
+    gdata <- c(beta, epsilon, gamma)
+    storage.mode(gdata) <- "double"
+    names(gdata) <- c("beta", "epsilon", "gamma")
+
+    model <- siminf_model(G      = G,
+                          S      = S,
+                          E      = E,
+                          N      = N,
+                          tspan  = tspan,
+                          events = events,
+                          ldata  = ldata,
+                          gdata  = gdata,
+                          u0     = u0,
+                          v0     = v0)
+
+    return(as(model, "SEIR"))
+}
+
+##' @rdname susceptible-methods
+##' @export
+setMethod("susceptible",
+          signature("SEIR"),
+          function(model, i = NULL, by = 1, ...) {
+              if (identical(dim(model@U), c(0L, 0L)))
+                  stop("Please run the model first, the 'U' matrix is empty")
+
+              ii <- seq(from = 1, to = dim(model@U)[1], by = 4)
+              if (!is.null(i))
+                  ii <- ii[i]
+              j <- seq(from = 1, to = dim(model@U)[2], by = by)
+              as.matrix(model@U[ii, j, drop = FALSE])
+          }
+)
+
+##' @rdname infected-methods
+##' @export
+setMethod("infected",
+          signature("SEIR"),
+          function(model, i = NULL, by = 1, ...) {
+              if (identical(dim(model@U), c(0L, 0L)))
+                  stop("Please run the model first, the 'U' matrix is empty")
+
+              ii <- seq(from = 3, to = dim(model@U)[1], by = 4)
+              if (!is.null(i))
+                  ii <- ii[i]
+              j <- seq(from = 1, to = dim(model@U)[2], by = by)
+              as.matrix(model@U[ii, j, drop = FALSE])
+          }
+)
+
+##' @rdname recovered-methods
+##' @export
+setMethod("recovered",
+          signature("SEIR"),
+          function(model, i = NULL, by = 1, ...) {
+              if (identical(dim(model@U), c(0L, 0L)))
+                  stop("Please run the model first, the 'U' matrix is empty")
+
+              ii <- seq(from = 4, to = dim(model@U)[1], by = 4)
+              if (!is.null(i))
+                  ii <- ii[i]
+              j <- seq(from = 1, to = dim(model@U)[2], by = by)
+              as.matrix(model@U[ii, j, drop = FALSE])
+          }
+)
+
+##' @name plot-methods
+##' @aliases plot plot-methods plot,SEIR-method
+##' @importFrom graphics plot
+##' @export
+setMethod("plot",
+          signature(x = "SEIR"),
+          function(x, t0 = NULL, ...)
+      {
+          callNextMethod(x, t0 = t0, legend = c("S", "E", "I", "R"), ...)
+      }
+)
+
+##' Scheduled events example data for the \code{SEIR} model
+##'
+##' Synthetic scheduled events data to demonstrate the \code{SEIR}
+##' model. The data contains 466692 events for 1600 nodes over 365 * 4
+##' days.
+##' @return A \code{data.frame}
+##' @keywords methods
+##' @importFrom utils data
+##' @export
+events_SEIR <- function() {
+    utils::data(events_SISe3, envir = environment())
+    events_SISe3$select[events_SISe3$event == 0] <- 2
+    events_SISe3$select[events_SISe3$event == 1] <- 1
+    events_SISe3 <- events_SISe3[events_SISe3$event != 2,]
+    events_SISe3$select[events_SISe3$event == 3] <- 2
+    events_SISe3
+}
+
+##' Example data to initialize the \code{SEIR} model
+##'
+##' Synthetic init data for 1600 nodes to demonstrate the \code{SEIR}
+##' model.
+##' @return A \code{data.frame}
+##' @keywords methods
+##' @importFrom utils data
+##' @export
+u0_SEIR <- function() {
+    utils::data(u0_SISe3, envir = environment())
+    u0_SISe3$S <- u0_SISe3$S_1 + u0_SISe3$S_2 + u0_SISe3$S_3
+    u0_SISe3$E <- 0
+    u0_SISe3$I <- u0_SISe3$I_1 + u0_SISe3$I_2 + u0_SISe3$I_3
+    u0_SISe3$R <- 0
+    u0_SISe3[, c("S", "E", "I", "R")]
+}
---
+++ tests/SEIR.R
@@ -0,0 +1,223 @@
+## SimInf, a framework for stochastic disease spread simulations
+## Copyright (C) 2015 - 2016  Stefan Widgren
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+library(SimInf)
+
+## For debugging
+sessionInfo()
+
+## Check invalid u0
+res <- tools::assertError(SEIR(u0 = "u0"))
+stopifnot(length(grep("'u0' must be a data.frame",
+                      res[[1]]$message)) > 0)
+
+u0 <- structure(list(S  = c(0, 1, 2, 3, 4, 5),
+                     E  = c(0, 0, 0, 0, 0, 0),
+                     I  = c(0, 0, 0, 0, 0, 0),
+                     R  = c(0, 0, 0, 0, 0, 0)),
+                .Names = c("S", "E", "I", "R"),
+                row.names = c(NA, -6L), class = "data.frame")
+
+## Check missing columns in u0
+res <- tools::assertError(SEIR(u0 = u0[, c("E", "I", "R"), drop = FALSE]))
+stopifnot(length(grep("Missing columns in u0",
+                      res[[1]]$message)) > 0)
+res <- tools::assertError(SEIR(u0 = u0[, c("S", "I", "R"), drop = FALSE]))
+stopifnot(length(grep("Missing columns in u0",
+                      res[[1]]$message)) > 0)
+res <- tools::assertError(SEIR(u0 = u0[, c("S", "E", "R"), drop = FALSE]))
+stopifnot(length(grep("Missing columns in u0",
+                      res[[1]]$message)) > 0)
+res <- tools::assertError(SEIR(u0 = u0[, c("S", "E", "I"), drop = FALSE]))
+stopifnot(length(grep("Missing columns in u0",
+                      res[[1]]$message)) > 0)
+
+## Check missing beta
+res <- tools::assertError(SEIR(u0      = u0,
+                               tspan   = seq_len(10) - 1,
+                               events  = NULL,
+                               epsilon = 0.5,
+                               gamma   = 0.5))
+stopifnot(length(grep("'beta' is missing",
+                      res[[1]]$message)) > 0)
+
+## Check missing epsilon
+res <- tools::assertError(SEIR(u0      = u0,
+                               tspan   = seq_len(10) - 1,
+                               events  = NULL,
+                               beta    = 0.5,
+                               gamma   = 0.5))
+stopifnot(length(grep("'epsilon' is missing",
+                      res[[1]]$message)) > 0)
+
+## Check missing gamma
+res <- tools::assertError(SEIR(u0      = u0,
+                               tspan   = seq_len(10) - 1,
+                               events  = NULL,
+                               beta    = 0.5,
+                               epsilon = 0.5))
+stopifnot(length(grep("'gamma' is missing",
+                      res[[1]]$message)) > 0)
+
+## Check non-numeric beta
+res <- tools::assertError(SEIR(u0      = u0,
+                               tspan   = seq_len(10) - 1,
+                               events  = NULL,
+                               beta    = "0.5",
+                               epsilon = 0.3,
+                               gamma   = 0.1))
+stopifnot(length(grep("'beta' must be numeric",
+                      res[[1]]$message)) > 0)
+
+## Check non-numeric epsilon
+res <- tools::assertError(SEIR(u0      = u0,
+                               tspan   = seq_len(10) - 1,
+                               events  = NULL,
+                               beta    = 0.5,
+                               epsilon = "0.3",
+                               gamma   = 0.1))
+stopifnot(length(grep("'epsilon' must be numeric",
+                      res[[1]]$message)) > 0)
+
+## Check non-numeric gamma
+res <- tools::assertError(SEIR(u0      = u0,
+                               tspan   = seq_len(10) - 1,
+                               events  = NULL,
+                               beta    = 0.5,
+                               epsilon = 0.3,
+                               gamma   = "0.1"))
+stopifnot(length(grep("'gamma' must be numeric",
+                      res[[1]]$message)) > 0)
+
+## Check that length of beta equals 1
+res <- tools::assertError(SEIR(u0      = u0,
+                               tspan   = seq_len(10) - 1,
+                               events  = NULL,
+                               beta    = c(0.5, 0.5),
+                               epsilon = 0.3,
+                               gamma   = 0.1))
+stopifnot(length(grep("'beta' must be of length 1",
+                      res[[1]]$message)) > 0)
+
+## Check that length of epsilon equals 1
+res <- tools::assertError(SEIR(u0      = u0,
+                               tspan   = seq_len(10) - 1,
+                               events  = NULL,
+                               beta    = 0.5,
+                               epsilon = c(0.3, 0.3),
+                               gamma   = 0.1))
+stopifnot(length(grep("'epsilon' must be of length 1",
+                      res[[1]]$message)) > 0)
+
+## Check that length of gamma equals 1
+res <- tools::assertError(SEIR(u0      = u0,
+                               tspan   = seq_len(10) - 1,
+                               events  = NULL,
+                               beta    = 0.5,
+                               epsilon = 0.3,
+                               gamma   = c(0.1, 0.1)))
+stopifnot(length(grep("'gamma' must be of length 1",
+                      res[[1]]$message)) > 0)
+
+## Check 'suscpetible', 'infected' and 'recovered' methods
+model <- SEIR(u0      = u0,
+              tspan   = seq_len(10) - 1,
+              events  = NULL,
+              beta    = 0,
+              epsilon = 0,
+              gamma   = 0)
+
+result <- run(model, threads = 1)
+
+S_expected <- structure(c(0L, 1L, 2L, 3L, 4L, 5L, 0L, 1L, 2L, 3L, 4L, 5L, 0L,
+                          1L, 2L, 3L, 4L, 5L, 0L, 1L, 2L, 3L, 4L, 5L, 0L, 1L,
+                          2L, 3L, 4L, 5L, 0L, 1L, 2L, 3L, 4L, 5L, 0L, 1L, 2L,
+                          3L, 4L, 5L, 0L, 1L, 2L, 3L, 4L, 5L, 0L, 1L, 2L, 3L,
+                          4L, 5L, 0L, 1L, 2L, 3L, 4L, 5L),
+                        .Dim = c(6L, 10L))
+
+S_observed <- susceptible(result)
+
+stopifnot(identical(S_observed, S_expected))
+
+I_expected <- structure(c(0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L,
+                          0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L,
+                          0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L,
+                          0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L,
+                          0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L),
+                        .Dim = c(6L, 10L))
+
+I_observed <- infected(result)
+
+stopifnot(identical(I_observed, I_expected))
+
+R_expected <- structure(c(0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L,
+                          0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L,
+                          0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L,
+                          0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L,
+                          0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L),
+                        .Dim = c(6L, 10L))
+
+R_observed <- recovered(result)
+
+stopifnot(identical(R_observed, R_expected))
+
+## Check data
+stopifnot(identical(nrow(events_SEIR()), 466692L))
+stopifnot(identical(nrow(u0_SEIR()), 1600L))
+
+## Check SEIR plot method
+pdf_file <- tempfile(fileext = ".pdf")
+pdf(pdf_file)
+plot(result)
+dev.off()
+stopifnot(file.exists(pdf_file))
+unlink(pdf_file)
+
+## Check SEIR events plot with no events
+model <- SEIR(u0      = u0,
+              tspan   = seq_len(10) - 1,
+              events  = NULL,
+              beta    = 0,
+              epsilon = 0,
+              gamma   = 0)
+pdf_file <- tempfile(fileext = ".pdf")
+pdf(pdf_file)
+plot(model@events)
+dev.off()
+stopifnot(file.exists(pdf_file))
+unlink(pdf_file)
+
+## Check SEIR events plot method
+model <- SEIR(u0      = u0_SEIR(),
+              tspan   = seq_len(365 * 4),
+              events  = events_SEIR(),
+              beta    = 0,
+              epsilon = 0,
+              gamma   = 0)
+pdf_file <- tempfile(fileext = ".pdf")
+pdf(pdf_file)
+plot(model@events)
+dev.off()
+stopifnot(file.exists(pdf_file))
+unlink(pdf_file)
+
+## Check that C SEIR run function fails for a misspecified SEIR model
+res <- .Call("SEIR_run", NULL, NULL, NULL, PACKAGE = "SimInf")
+stopifnot(identical(res$error, -10L))
+
+res <- .Call("SEIR_run", "SEIR", NULL, NULL, PACKAGE = "SimInf")
+stopifnot(identical(res$error, -10L))
