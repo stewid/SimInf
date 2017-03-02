6b346339a3b5263202063fcd98bc2546937dc297
Stefan Widgren <stefan.widgren@gmail.com>
2017-03-02 11:34:57 +0100

    Improve documentation of end_t arguments

--- man-roxygen/beta-param.R
+++ man-roxygen/beta-param.R
@@ -1,12 +1,16 @@
 ##' @param beta_t1 The decay of the environmental infectious pressure
-##' in interval 1.
+##'     in interval 1.
 ##' @param beta_t2 The decay of the environmental infectious pressure
-##' in interval 2.
+##'     in interval 2.
 ##' @param beta_t3 The decay of the environmental infectious pressure
-##' in interval 3.
+##'     in interval 3.
 ##' @param beta_t4 The decay of the environmental infectious pressure
-##' in interval 4.
-##' @param end_t1 The non-inclusive day that ends interval 1.
-##' @param end_t2 The non-inclusive day that ends interval 2.
-##' @param end_t3 The non-inclusive day that ends interval 3.
-##' @param end_t4 The non-inclusive day that ends interval 4.
+##'     in interval 4.
+##' @param end_t1 The non-inclusive day of the year that ends interval
+##'     1.
+##' @param end_t2 The non-inclusive day of the year that ends interval
+##'     2.
+##' @param end_t3 The non-inclusive day of the year that ends interval
+##'     3.
+##' @param end_t4 The non-inclusive day of the year that ends interval
+##'     4.
