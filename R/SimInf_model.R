63a74b48873e889ad3ce15a9a1b800fcfe9bd008
Stefan Widgren <stefan.widgren@gmail.com>
2017-03-19 15:56:05 +0100

    Add documentation for the SimInf_model C_code slot

--- R/SimInf_model.R
+++ R/SimInf_model.R
@@ -92,6 +92,13 @@
 ##'   \item{events}{
 ##'     Scheduled events \code{"\linkS4class{scheduled_events}"}
 ##'   }
+##'   \item{C_code}{
+##'     Character vector with optional model C code. If non-empty, the
+##'     C code is written to a temporary C-file when the \code{run}
+##'     method is called.  The temporary C-file is compiled and the
+##'     resulting DLL is dynamically loaded. The DLL is unloaded and
+##'     the temporary files are removed after running the model.
+##'   }
 ##' }
 ##' @include scheduled_events.R
 ##' @keywords methods
