6edc69db182341236f6abdfd6768164fa09ff344
Stefan Widgren <stefan.widgren@gmail.com>
2017-12-04 21:00:41 +0100

    Better example in the documentation for the 'u0_SISe' method

--- R/SISe.R
+++ R/SISe.R
@@ -197,12 +197,50 @@ events_SISe <- function() {
     events_SISe3
 }
 
-##' Example data to initialize the \code{SISe} model
+##' Example data to initialize the \sQuote{SISe} model
 ##'
-##' Synthetic init data for 1600 nodes to demonstrate the \code{SISe}
-##' model.
+##' Example data to initialize a population of 1600 nodes and
+##' demonstrate the \code{\linkS4class{SISe}} model.
+##'
+##' A \code{data.frame} with the number of individuals in the
+##' \sQuote{S} and \sQuote{I} compartments in 1600 nodes. Note that
+##' the \sQuote{I} compartment is zero.
 ##' @return A \code{data.frame}
 ##' @export
+##' @examples
+##' ## Create an 'SISe' model with 1600 nodes and initialize it to
+##' ## run over 4*365 days and record data at weekly time-points.
+##'
+##' ## Load the initial population and add ten infected individuals to
+##' ## the first node.
+##' u0 <- u0_SISe()
+##' u0$I[1] <- 10
+##'
+##' ## Define 'tspan' to run the simulation over 4*365 and record the
+##' ## state of the system at weekly time-points.
+##' tspan <- seq(from = 1, to = 4*365, by = 7)
+##'
+##' ## Load scheduled events for the population of nodes with births,
+##' ## deaths and between-node movements of individuals.
+##' events <- events_SISe()
+##'
+##' ## Create a 'SISe' model
+##' model <- SISe(u0 = u0, tspan = tspan, events = events,
+##'               phi = rep(0, nrow(u0)), upsilon = 1.8e-2,
+##'               gamma = 0.1, alpha = 1, beta_t1 = 1.0e-1,
+##'               beta_t2 = 1.0e-1, beta_t3 = 1.25e-1,
+##'               beta_t4 = 1.25e-1, end_t1 = 91, end_t2 = 182,
+##'               end_t3 = 273, end_t4 = 365, epsilon = 0)
+##'
+##' ## Run the model to generate a single stochastic trajectory.
+##' result <- run(model, threads = 1, seed = 22)
+##'
+##' ## Summarize trajectory
+##' summary(result)
+##'
+##' ## Plot the proportion of nodes with at least one infected
+##' ## individual.
+##' plot(prevalence(result, I~S+I, "nop"), type = "l")
 u0_SISe <- function() {
     utils::data("u0_SISe3", package = "SimInf", envir = environment())
     u0_SISe3$S <- u0_SISe3$S_1 + u0_SISe3$S_2 + u0_SISe3$S_3
