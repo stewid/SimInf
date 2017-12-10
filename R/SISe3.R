247479ba6595d4e9857490168f74c89a8736fa76
Stefan Widgren <stefan.widgren@gmail.com>
2017-12-10 15:45:26 +0100

    Better documentation of the 'events_SISe3' dataset

--- R/SISe3.R
+++ R/SISe3.R
@@ -228,3 +228,105 @@ setMethod("plot",
                                       lty = lty, lwd = lwd, ...)
           }
 )
+
+##' Example data to initialize events for the \sQuote{SISe3} model
+##'
+##' Example data to initialize scheduled events for a population of
+##' 1600 nodes and demonstrate the \code{\linkS4class{SISe3}} model.
+##'
+##' Example data to initialize scheduled events (see
+##' \code{\linkS4class{SimInf_events}}) for a population of 1600 nodes
+##' and demonstrate the \code{\linkS4class{SISe3}} model. The dataset
+##' contains 783773 events for 1600 nodes distributed over 4 * 365
+##' days. The events are divided into three types: \sQuote{Exit}
+##' events remove individuals from the population (n = 182535),
+##' \sQuote{Enter} events add individuals to the population (n =
+##' 182685), sQuote{Internal transfer} events move individuals between
+##' compartmens within one node e.g. ageing (n = 317081), and
+##' \sQuote{External transfer} events move individuals between nodes
+##' in the population (n = 101472). The vignette contains a detailed
+##' description of how scheduled events operate on a model.
+##' @name events_SISe3
+##' @docType data
+##' @usage data(events_SISe3)
+##' @format A \code{data.frame}
+##' @keywords dataset
+##' @examples
+##' ## Create an 'SISe3' model with 1600 nodes and initialize
+##' ## it to run over 4*365 days. Add one infected individual
+##' ## to the first node.
+##' data("u0_SISe3", package = "SimInf")
+##' data("events_SISe3", package = "SimInf")
+##' u0_SISe3$I_1[1] <- 1
+##' tspan <- seq(from = 1, to = 4*365, by = 1)
+##' model <- SISe3(u0 = u0_SISe3, tspan = tspan, events = events_SISe3,
+##'                phi = rep(0, nrow(u0_SISe3)), upsilon_1 = 1.8e-2,
+##'                upsilon_2 = 1.8e-2, upsilon_3 = 1.8e-2,
+##'                gamma_1 = 0.1, gamma_2 = 0.1, gamma_3 = 0.1,
+##'                alpha = 1, beta_t1 = 1.0e-1, beta_t2 = 1.0e-1,
+##'                beta_t3 = 1.25e-1, beta_t4 = 1.25e-1, end_t1 = 91,
+##'                end_t2 = 182, end_t3 = 273, end_t4 = 365, epsilon = 0)
+##'
+##' ## Display the number of individuals affected by each event type
+##' ## per day.
+##' plot(events(model))
+##'
+##' ## Run the model to generate a single stochastic trajectory.
+##' result <- run(model, threads = 1, seed = 3)
+##'
+##' ## Summarize the trajectory. The summary includes the number of
+##' ## events by event type.
+##' summary(result)
+NULL
+
+##' Example data to initialize the \sQuote{SISe3} model
+##'
+##' Example data to initialize a population of 1600 nodes and
+##' demonstrate the \code{\linkS4class{SISe3}} model.
+##'
+##' A \code{data.frame} with the number of individuals in the
+##' \sQuote{S_1}, \sQuote{S_2}, \sQuote{S_3}, \sQuote{I_1},
+##' \sQuote{I_2} and \sQuote{I_3} compartments in 1600 nodes. Note
+##' that the \sQuote{I_1}, \sQuote{I_2} and \sQuote{I_3} compartments
+##' are zero.
+##' @name u0_SISe3
+##' @docType data
+##' @usage data(u0_SISe3)
+##' @format A \code{data.frame}
+##' @keywords dataset
+##' @examples
+##' ## Create an 'SISe3' model with 1600 nodes and initialize it to
+##' ## run over 4*365 days and record data at weekly time-points.
+##'
+##' ## Load the initial population and add ten infected individuals to
+##' ## I_1 in the first node.
+##' u0 <- u0_SISe3
+##' u0$I_1[1] <- 10
+##'
+##' ## Define 'tspan' to run the simulation over 4*365 and record the
+##' ## state of the system at weekly time-points.
+##' tspan <- seq(from = 1, to = 4*365, by = 7)
+##'
+##' ## Load scheduled events for the population of nodes with births,
+##' ## deaths and between-node movements of individuals.
+##' events <- events_SISe3
+##'
+##' ## Create a 'SISe3' model
+##' model <- SISe3(u0 = u0, tspan = tspan, events = events,
+##'                phi = rep(0, nrow(u0)), upsilon_1 = 1.8e-2,
+##'                upsilon_2 = 1.8e-2, upsilon_3 = 1.8e-2,
+##'                gamma_1 = 0.1, gamma_2 = 0.1, gamma_3 = 0.1,
+##'                alpha = 1, beta_t1 = 1.0e-1, beta_t2 = 1.0e-1,
+##'                beta_t3 = 1.25e-1, beta_t4 = 1.25e-1, end_t1 = 91,
+##'                end_t2 = 182, end_t3 = 273, end_t4 = 365, epsilon = 0)
+##'
+##' ## Run the model to generate a single stochastic trajectory.
+##' result <- run(model, threads = 1, seed = 22)
+##'
+##' ## Summarize trajectory
+##' summary(result)
+##'
+##' ## Plot the proportion of nodes with at least one infected
+##' ## individual.
+##' plot(prevalence(result, I_1 + I_2 + I_3 ~ ., "nop"), type = "l")
+NULL
