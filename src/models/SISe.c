07d1f423da1b41b6f3f4115565a81fbd97aee251
Stefan Widgren <stefan.widgren@gmail.com>
2020-01-17 18:14:48 +0100

    Remove dexription of non-existing parameter from documentation

--- src/models/SISe.c
+++ src/models/SISe.c
@@ -5,7 +5,7 @@
  * Copyright (C) 2015 Pavol Bauer
  * Copyright (C) 2017 -- 2019 Robin Eriksson
  * Copyright (C) 2015 -- 2019 Stefan Engblom
- * Copyright (C) 2015 -- 2019 Stefan Widgren
+ * Copyright (C) 2015 -- 2020 Stefan Widgren
  *
  * SimInf is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -87,7 +87,6 @@ double SISe_I_to_S(
  * @param gdata The global data vector.
  * @param node The node.
  * @param t The current time.
- * @param rng The random number generator.
  * @return error code (<0), or 1 if node needs to update the
  * transition rates, or 0 when it doesn't need to update the
  * transition rates.
