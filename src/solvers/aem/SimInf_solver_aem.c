d7458299a20d8a7e2483d8849856596d92e64b73
Stefan Widgren <stefan.widgren@gmail.com>
2021-01-16 11:28:57 +0100

    Update the search path for header files

--- src/solvers/aem/SimInf_solver_aem.c
+++ src/solvers/aem/SimInf_solver_aem.c
@@ -5,7 +5,7 @@
  * Copyright (C) 2015 Pavol Bauer
  * Copyright (C) 2017 -- 2019 Robin Eriksson
  * Copyright (C) 2015 -- 2019 Stefan Engblom
- * Copyright (C) 2015 -- 2020 Stefan Widgren
+ * Copyright (C) 2015 -- 2021 Stefan Widgren
  *
  * SimInf is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -29,9 +29,9 @@
 #include <gsl/gsl_randist.h>
 
 #include "SimInf.h"
-#include "SimInf_openmp.h"
+#include "misc/SimInf_openmp.h"
 #include "SimInf_solver_aem.h"
-#include "binheap.h"
+#include "misc/binheap.h"
 
 /**
  * Structure to hold AEM solver specific data/arguments for simulation.
