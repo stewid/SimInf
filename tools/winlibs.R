55d5f0f504459106790c7ded141efdad4725402b
Stefan Widgren <stefan.widgren@gmail.com>
2021-01-17 16:12:00 +0100

    Windows: refactoring of the build script

--- tools/winlibs.R
+++
@@ -1,8 +0,0 @@
-VERSION <- commandArgs(TRUE)
-if (!file.exists(sprintf("../windows/gsl-%s/include/gsl/gsl_rng.h", VERSION))) {
-    url <- sprintf("https://github.com/rwinlib/gsl/archive/v%s.zip", VERSION)
-    download.file(url, "lib.zip", quiet = TRUE)
-    dir.create("../windows", showWarnings = FALSE)
-    unzip("lib.zip", exdir = "../windows")
-    unlink("lib.zip")
-}
