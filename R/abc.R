15b0e5826fc48188ae53501a17bb5b569f0a0668
Stefan Widgren <stefan.widgren@gmail.com>
2021-03-20 22:20:14 +0100

    documentation: create templates for priors slot

--- R/abc.R
+++ R/abc.R
@@ -20,17 +20,7 @@
 ##'
 ##' @slot model The \code{SimInf_model} object to estimate parameters
 ##'     in.
-##' @slot priors A \code{data.frame} containing the four columns
-##'     \code{parameter}, \code{distribution}, \code{p1} and
-##'     \code{p2}. The column \code{parameter} gives the name of the
-##'     parameter referred to in the model. The column
-##'     \code{distribution} contains a letter indicating the prior
-##'     distribution. Valid letters are 'G' (gamma), 'N' (normal) or
-##'     'U' (uniform). The column \code{p1} is a numeric vector with
-##'     the first hyperparameter for each prior: 'G') shape, 'N')
-##'     mean, and 'U') lower bound. The column \code{p2} is a numeric
-##'     vector with the second hyperparameter for each prior: 'G')
-##'     rate, 'N') standard deviation, and 'U') upper bound.
+##' @template priors-slot
 ##' @slot target Character vector (\code{gdata} or \code{ldata}) that
 ##'     determines if the ABC-SMC method estimates parameters in
 ##'     \code{model@@gdata} or in \code{model@@ldata}.
