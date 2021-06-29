6a738e9b9c09d866f2c6998e55a83f9941517ced
Stefan Widgren <stefan.widgren@sva.se>
2021-06-29 15:16:32 +0200

    Remove tools/winlibs.R

--- tools/winlibs.R
+++
@@ -1,7 +0,0 @@
-# Download GSL 2.7 Rtools build
-if (!file.exists("../windows/gsl-2.7/include/gsl/gsl_blas.h")) {
-    download.file("https://github.com/rwinlib/gsl/archive/v2.7.zip", "lib.zip", quiet = TRUE)
-    dir.create("../windows", showWarnings = FALSE)
-    unzip("lib.zip", exdir = "../windows")
-    unlink("lib.zip")
-}
