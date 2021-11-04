8292e373e26ac843599532e5587995c5986ba5ad
Stefan Widgren <stefan.widgren@gmail.com>
2021-11-04 23:06:11 +0100

    pfilter: add test

--- tests/pfilter.R
+++ tests/pfilter.R
@@ -365,3 +365,26 @@ stopifnot(identical(
          expr = "stats::dbinom(x = Iobs, size = 100, prob = I/100, log = TRUE)",
          par = "Iobs",
          par_i = 2L)))
+
+result <- SimInf:::pfilter_obs_process(
+                       SIR(u0 = data.frame(S = 99, I = 1, R = 0),
+                           tspan = seq(1, 21, by = 3),
+                           beta = 0.16,
+                           gamma = 0.077),
+                       Iobs ~ uniform(I - 3, I + 3),
+                       list("1" = data.frame(time = 1, Iobs = 1),
+                            "4" = data.frame(time = 4, Iobs = 2),
+                            "7" = data.frame(time = 7, Iobs = 2),
+                            "10" = data.frame(time = 10, Iobs = 3),
+                            "13" = data.frame(time = 13, Iobs = 3),
+                            "16" = data.frame(time = 16, Iobs = 3),
+                            "19" = data.frame(time = 19, Iobs = 3)),
+                       5)
+
+stopifnot(identical(
+    result,
+    list(slots = list(list(slot = "U", name = "I", i = c(2, 5, 8, 11, 14)),
+                      list(slot = "U", name = "I", i = c(2, 5, 8, 11, 14))),
+         expr = "stats::dunif(x = Iobs, min = I-3, max = I+3, log = TRUE)",
+         par = "Iobs",
+         par_i = 2L)))
