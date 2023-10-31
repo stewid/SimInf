8320119ab3d593445cf1fd4d7716e24f4e07e88a
Stefan Widgren <stefan.widgren@gmail.com>
2023-10-31 07:30:28 +0100

    Add missing backslash

--- man-roxygen/post_gen-param.R
+++ man-roxygen/post_gen-param.R
@@ -1,5 +1,5 @@
 ##' @param post_gen An optional function that, if non-NULL, is applied
 ##'     after each completed generation. The function must accept one
-##'     argument of type code{SimInf_abc} with the current state of
+##'     argument of type \code{SimInf_abc} with the current state of
 ##'     the fitting process. This function can be useful to, for
 ##'     example, save and inspect intermediate results.
