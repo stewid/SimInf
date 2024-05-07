943dcf6ebe3c2eb76253e0850012afa88fb70261
Stefan Widgren <stefan.widgren@sva.se>
2024-05-07 17:42:51 +0200

    pmcmc: at least one accepted proposal before using empirical covariance

--- R/pmcmc.R
+++ R/pmcmc.R
@@ -355,7 +355,7 @@ get_theta <- function(x, i) {
 pmcmc_proposal <- function(x, i, n_accepted, theta_mean, covmat_emp,
                            scale_start = 100L, shape_start = 200L,
                            cooling = 0.999, max_scaling = 50) {
-    if (runif(1) < x@adaptmix || i <= scale_start) {
+    if (runif(1) < x@adaptmix || i <= scale_start || n_accepted == 0) {
         covmat <- diag((get_theta(x, 1) / 10)^2 / n_pars(x))
     } else if (n_accepted < shape_start) {
         scaling <- 1
