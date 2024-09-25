02fca58dc5b330830e83fc59bc91515f22116372
Stefan Widgren <stefan.widgren@gmail.com>
2024-09-25 19:31:52 +0200

    Add roxygen template file

---
+++ man-roxygen/verbose-param-pmcmc.R
@@ -0,0 +1,7 @@
+##' @param verbose prints diagnostic messages when \code{TRUE}. The
+##'     default is to retrieve the global option \code{verbose} and
+##'     use \code{FALSE} if it is not set. When \code{verbose=TRUE},
+##'     information is printed every 100 iterations. For pmcmc, it is
+##'     possible to get information every nth information by
+##'     specifying \code{verbose=n}, for example, \code{verbose=1} or
+##'     \code{verbose=10}.
