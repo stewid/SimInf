8e6641d4a14c2069e2eaeba79287fbc8f4c3818c
Stefan Widgren <stefan.widgren@gmail.com>
2024-11-10 09:49:18 +0100

    mssm: check for error in each iteration

--- src/solvers/mssm/SimInf_solver_mssm.c
+++ src/solvers/mssm/SimInf_solver_mssm.c
@@ -115,7 +115,7 @@ SimInf_solver_mssm(
                 }
 
                 /* Main loop. */
-                for (;;) {
+                for (;!m.error;) {
                     /* (1) Handle internal epidemiological model,
                      * continuous-time Markov chain. */
                     for (int node = 0; node < m.Nn && !m.error; node++) {
@@ -231,7 +231,7 @@ SimInf_solver_mssm(
                      * each timestep e.g. update the infectious
                      * pressure variable. Moreover, update transition
                      * rates in nodes that are indicated for update */
-                    for (int node = 0; node < m.Nn; node++) {
+                    for (int node = 0; node < m.Nn && !m.error; node++) {
                         const int rc = m.pts_fun(
                             &m.v_new[node * m.Nd],
                             &m.u[node * m.Nc],
@@ -327,9 +327,9 @@ SimInf_solver_mssm(
     model[0].v_new = v_new;
 
     /* Check for error. */
-    for (int k = 0; k < Nthread; k++)
-        if (model[k].error)
-            return model[k].error;
+    for (int j = 0; j < Nthread; j++)
+        if (model[j].error)
+            return model[j].error;
 
     return 0;
 }
