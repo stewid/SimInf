975a81f1a42f05e413f5152717cf3c06f8f5a124
Stefan Widgren <stefan.widgren@gmail.com>
2025-01-29 07:03:54 +0100

    Documentation

--- man-roxygen/post_proposal-param.R
+++ man-roxygen/post_proposal-param.R
@@ -1,7 +1,9 @@
-##' @param pre_particle An optional function that, if non-NULL, is
-##'     applied on the model after the proposal has been set for the
-##'     model, but before running the particle filter. The function
-##'     must accept one argument of type \code{SimInf_model} with the
-##'     current model of the fitting process.  This function can be
-##'     useful to update, for example, \code{ldata} of the model
-##'     before running a trajectory with proposed parameters.
+##' @param post_proposal An optional function that, if
+##'     non-\code{NULL}, is applied on the model after the proposal
+##'     has been set for the model, but before running the particle
+##'     filter. The function must accept one argument of type
+##'     \code{SimInf_model} with the current model of the fitting
+##'     process. This function can be useful to update, for example,
+##'     \code{ldata} of the model before running a trajectory with
+##'     proposed parameters. The function must return the model object
+##'     which is then used in the particle filter.
