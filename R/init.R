f2fa888bd8525babe482baaffbf0ee310749f16f
Stefan Widgren <stefan.widgren@gmail.com>
2025-04-10 07:07:10 +0200

    Update copyright year

--- R/init.R
+++ R/init.R
@@ -4,7 +4,7 @@
 ## Copyright (C) 2015 Pavol Bauer
 ## Copyright (C) 2017 -- 2019 Robin Eriksson
 ## Copyright (C) 2015 -- 2019 Stefan Engblom
-## Copyright (C) 2015 -- 2022 Stefan Widgren
+## Copyright (C) 2015 -- 2025 Stefan Widgren
 ##
 ## SimInf is free software: you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
