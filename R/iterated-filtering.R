128ade0ea20197ec0555f66fddbf79616c9b6837
Stefan Widgren <stefan.widgren@gmail.com>
2025-04-13 14:50:15 +0200

    if2: initial work to add class SimInf_if2

---
+++ R/iterated-filtering.R
@@ -0,0 +1,29 @@
+## This file is part of SimInf, a framework for stochastic
+## disease spread simulations.
+##
+## Copyright (C) 2015 -- 2025 Stefan Widgren
+##
+## SimInf is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## SimInf is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+##' Class \code{"SimInf_if2"}
+##'
+##' @slot model The \code{SimInf_model} object to estimate parameters
+##'     in.
+##' @template priors-slot
+##' @export
+setClass(
+    "SimInf_if2",
+    slots = c(model = "SimInf_model",
+              priors = "data.frame")
+)
