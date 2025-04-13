55262b975c6112d3364ac527a51aedbe5ac8bdc3
Stefan Widgren <stefan.widgren@gmail.com>
2025-04-13 17:58:20 +0200

    Update copyright year

--- R/u0.R
+++ R/u0.R
@@ -1,7 +1,7 @@
 ## This file is part of SimInf, a framework for stochastic
 ## disease spread simulations.
 ##
-## Copyright (C) 2015 -- 2024 Stefan Widgren
+## Copyright (C) 2015 -- 2025 Stefan Widgren
 ##
 ## SimInf is free software: you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
