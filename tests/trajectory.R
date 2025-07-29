fc0cd03c86c43f746da0590e7311d6dfad3cd8b8
Stefan Widgren <stefan.widgren@gmail.com>
2025-07-29 17:10:38 +0200

    trajectory: add test

--- tests/trajectory.R
+++ tests/trajectory.R
@@ -700,3 +700,62 @@ df_observed <- .Call(
     "node",
     1L)
 stopifnot(identical(df_observed, df_expected))
+
+## 1 sparse discrete matrix.
+## 1 sparse continuous matrix.
+## Subset of identifiers.
+## 1 replicate.
+dm <- matrix(1:180, nrow = 18, ncol = 10)
+dm[c(4, 5, 6, 13, 14, 15), ] <- 0L
+dm[, 1] <- 0L
+dm[2, 2] <- 0L
+dm[3, 3] <- 0L
+dm[7, 4] <- 0L
+dm[8, 5] <- 0L
+dm[9, 6] <- 0L
+dm[10, 7] <- 0L
+dm[11, 8] <- 0L
+dm[12, 9] <- 0L
+dm[16, 10] <- 0L
+dm <- as(as(as(Matrix(dm), "dMatrix"), "generalMatrix"), "CsparseMatrix")
+
+df_expected <- data.frame(
+    node = c(1L, 3L, 6L, 1L, 3L, 6L, 1L, 3L, 6L, 1L, 3L, 6L,
+             1L, 3L, 6L, 1L, 3L, 6L, 1L, 3L, 6L, 1L, 3L, 6L,
+             1L, 3L, 6L, 1L, 3L, 6L),
+    time = c(1L, 1L, 1L, 2L, 2L, 2L, 3L, 3L, 3L, 4L, 4L, 4L,
+             5L, 5L, 5L, 6L, 6L, 6L, 7L, 7L, 7L, 8L, 8L, 8L,
+             9L, 9L, 9L, 10L, 10L, 10L),
+    S = c(NA, NA, NA, 19L, 25L, 34L, 37L, 43L, 52L, 55L, NA,
+          70L, 73L, 79L, 88L, 91L, 97L, 106L, 109L, 115L, 124L,
+          127L, 133L, 142L, 145L, 151L, 160L, 163L, 169L, NA),
+    I = c(NA, NA, NA, NA, 26L, 35L, 38L, 44L, 53L, 56L, 62L, 71L,
+          74L, NA, 89L, 92L, 98L, 107L, 110L, 116L, 125L, 128L,
+          134L, 143L, 146L, 152L, 161L, 164L, 170L, 179L),
+    R = c(NA, NA, NA, 21L, 27L, 36L, NA, 45L, 54L, 57L, 63L, 72L,
+          75L, 81L, 90L, 93L, NA, 108L, 111L, 117L, 126L, 129L,
+          135L, 144L, 147L, 153L, 162L, 165L, 171L,180L),
+    alpha = c(NA, 1007, 1016, 1019, 1025, 1034, 1037, 1043, 1052,
+              1055, NA, 1070, 1073, 1079, 1088, 1091, 1097, 1106,
+              1109, 1115, 1124, 1127, 1133, 1142, 1145, 1151, 1160,
+              1163, 1169, NA),
+    beta = c(1002, 1008, NA, NA, 1026, 1035, 1038, 1044, 1053, 1056,
+             1062, 1071, 1074, NA, 1089, 1092, 1098, 1107, 1110, 1116,
+             1125, 1128, 1134, 1143, 1146, 1152, 1161, 1164, 1170, 1179),
+    gamma = c(1003, 1009, NA, 1021, 1027, 1036, NA, 1045, 1054, 1057,
+              1063, 1072, 1075, 1081, 1090, 1093, NA, 1108, 1111, 1117,
+              1126, 1129, 1135, 1144, 1147, 1153, 1162, 1165, 1171, 1180))
+df_observed <- .Call(
+    SimInf:::SimInf_trajectory,
+    dm,
+    1:3,
+    c("S", "I", "R"),
+    cm,
+    1:3,
+    c("alpha", "beta", "gamma"),
+    as.numeric(1:10),
+    6L,
+    c(1L, 3L, 6L),
+    "node",
+    1L)
+stopifnot(identical(df_observed, df_expected))
