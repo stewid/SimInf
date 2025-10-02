c5d52d777f1a50c64ff726221e24a48f4502ca62
Stefan Widgren <stefan.widgren@sva.se>
2025-10-02 15:59:02 +0200

    Add 'ldata_add_spatial_coupling' function

--- R/distance.R
+++ R/distance.R
@@ -4,7 +4,7 @@
 ## Copyright (C) 2015 Pavol Bauer
 ## Copyright (C) 2017 -- 2019 Robin Eriksson
 ## Copyright (C) 2015 -- 2019 Stefan Engblom
-## Copyright (C) 2015 -- 2024 Stefan Widgren
+## Copyright (C) 2015 -- 2025 Stefan Widgren
 ##
 ## SimInf is free software: you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
@@ -54,4 +54,41 @@ distance_matrix <- function(x, y, cutoff, min_dist = NULL, na_fail = TRUE) {
           as.numeric(cutoff),
           as.numeric(min_dist),
           isTRUE(na_fail))
+}
+
+check_metric <- function(metric) {
+    check_integer_arg(metric)
+    metric <- as.integer(metric)
+    if (any(length(metric) != 1L, any(metric < 0L), any(metric > 2L)))
+        stop("'metric' must be an integer 0, 1, or 2.", call. = FALSE)
+    metric
+}
+
+##' Add information about spatial coupling between nodes to 'ldata'
+##'
+##' A utility function to combine local model parameters
+##' (\code{ldata}) and spatial coupling to other nodes and add the
+##' result to \code{ldata}.
+##'
+##' Format for ldata: the first n indicies (1, 2, ..., n) are the
+##' local model parameters, i.e, the indata to the function. They are
+##' followed by the neighbor data, pairs of (index, value) and then a
+##' stop pair (-1, 0) where 'index' is the zero-based index to the
+##' neighbor and the value is determined by the metric argument.
+##' @param ldata Matrix with local model parameters for each node.
+##' @param distance Sparse matrix
+##'     (\code{\link[Matrix:dgCMatrix-class]{dgCMatrix}}) with
+##'     distances between nodes, see \code{\link{distance_matrix}}.
+##' @param metric The type of value to calculate for each neighbor: 0)
+##'     degree, 1) distance, and 2) 1 / (distance * distance). Default
+##'     is \code{1}.
+##' @return matrix
+##' @export
+##' @examples
+##' d <- distance_matrix(nodes$x, nodes$y, cutoff = 5000)
+##' ldata <- rbind(nodes$x, nodes$y)
+##' ldata <- ldata_add_spatial_coupling(ldata, d)
+ldata_add_spatial_coupling <- function(ldata, distance, metric = 1) {
+    metric <- check_metric(metric)
+    .Call(SimInf_ldata_sp, ldata, distance, metric)
 }
