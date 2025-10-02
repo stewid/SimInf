29545586044c29611dc8489b3929d03fddeeae65
Stefan Widgren <stefan.widgren@gmail.com>
2025-10-02 22:03:00 +0200

    Documentation

--- R/lambert.R
+++ R/lambert.R
@@ -16,10 +16,25 @@
 ## You should have received a copy of the GNU General Public License
 ## along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-##' Lambert W Function
+##' Lambert W0 function
 ##'
+##' W0(x) is the principal branch of the solution of the function
+##' defined by \eqn{We^W = x}{W * exp(W)} for \eqn{x >= -1/e}. The
+##' value is calculated using GNU Scientific Library (GSL).
 ##' @param x numeric vector of values.
+##' @references
+##'
+##' GNU Scientific Library <https://www.gnu.org/software/gsl/>
 ##' @export
+##' @examples
+##' ## Should equal 1, as 1 * exp(1) = e.
+##' lambertW0(exp(1))
+##'
+##' ## Should equal 0, as 0 * exp(0) = 0.
+##' lambertW0(0)
+##'
+##' ## Should equal -1.
+##' lambertW0(-exp(-1))
 lambertW0 <- function(x) {
     .Call(SimInf_lambertW0, x)
 }
