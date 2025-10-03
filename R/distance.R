886c279dc3d8076779c39740ba41d09612db5aa0
Stefan Widgren <stefan.widgren@gmail.com>
2025-10-03 07:48:33 +0200

    Refactoring of 'ldata_add_spatial_coupling' function to simplify usage

--- R/distance.R
+++ R/distance.R
@@ -56,14 +56,6 @@ distance_matrix <- function(x, y, cutoff, min_dist = NULL, na_fail = TRUE) {
           isTRUE(na_fail))
 }
 
-check_metric <- function(metric) {
-    check_integer_arg(metric)
-    metric <- as.integer(metric)
-    if (any(length(metric) != 1L, any(metric < 0L), any(metric > 2L)))
-        stop("'metric' must be an integer 0, 1, or 2.", call. = FALSE)
-    metric
-}
-
 ##' Add information about spatial coupling between nodes to 'ldata'
 ##'
 ##' A utility function to combine local model parameters
@@ -75,20 +67,35 @@ check_metric <- function(metric) {
 ##' followed by the neighbor data, pairs of (index, value) and then a
 ##' stop pair (-1, 0) where 'index' is the zero-based index to the
 ##' neighbor and the value is determined by the metric argument.
+##' @param x Projected x coordinate
+##' @param y Projected y coordinate
+##' @param cutoff The distance cutoff
 ##' @param ldata Matrix with local model parameters for each node.
-##' @param distance Sparse matrix
-##'     (\code{\link[Matrix:dgCMatrix-class]{dgCMatrix}}) with
-##'     distances between nodes, see \code{\link{distance_matrix}}.
-##' @param metric The type of value to calculate for each neighbor: 0)
-##'     degree, 1) distance, and 2) 1 / (distance * distance). Default
-##'     is \code{1}.
+##' @param min_dist The minimum distance to separate two nodes.  If
+##'     the coordinates for two nodes are identical, the min_dist must
+##'     be assigned or an error is raised.  Default is \code{NULL},
+##'     i.e., to raise an error.
+##' @param na_fail A logical indicating whether missing values in
+##'     \code{x} or \code{y} should raise an error or assign zero to
+##'     all distances involving missing values.  Default is
+##'     \code{TRUE}, i.e., to raise an error.
 ##' @return matrix
 ##' @export
 ##' @examples
-##' d <- distance_matrix(nodes$x, nodes$y, cutoff = 5000)
-##' ldata <- rbind(nodes$x, nodes$y)
-##' ldata <- ldata_add_spatial_coupling(ldata, d)
-ldata_add_spatial_coupling <- function(ldata, distance, metric = 1) {
-    metric <- check_metric(metric)
-    .Call(SimInf_ldata_sp, ldata, distance, metric)
+##' ldata <- ldata_add_spatial_coupling(nodes$x, nodes$y, cutoff = 5000)
+ldata_add_spatial_coupling <- function(x, y, cutoff, ldata = NULL,
+                                       min_dist = NULL, na_fail = TRUE) {
+    distance <- distance_matrix(x = x,
+                                y = y,
+                                cutoff = cutoff,
+                                min_dist = min_dist,
+                                na_fail = na_fail)
+
+    if (is.null(ldata)) {
+        ldata <- matrix(data = numeric(), nrow = 0, ncol = dim(distance)[1])
+    } else {
+        ldata <- init_data_matrix(ldata)
+    }
+
+    .Call(SimInf_ldata_sp, ldata, distance, 1L)
 }
