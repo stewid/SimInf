d62b8afc973eafdd5dd36e062c2fd2c1464abc19
Stefan Widgren <stefan.widgren@gmail.com>
2026-04-03 16:33:35 +0200

    Documentation

--- man/examples/SIR.R
+++ man/examples/SIR.R
@@ -5,19 +5,19 @@
 set.seed(123)
 set_num_threads(1)
 
-## Create a 'SEIR' model with 1600 cattle herds (nodes) and initialize
-## it to run over 4*365 days. Add ten exposed animals to the first
-## herd. Define 'tspan' to record the state of the system at weekly
-## time-points. Load scheduled events for the population of nodes with
-## births, deaths and between-node movements of individuals.
-u0 <- u0_SEIR()
-u0$E[1] <- 10
-model <- SEIR(u0      = u0,
-              tspan   = seq(from = 1, to = 4*365, by = 7),
-              events  = events_SEIR(),
-              beta    = 0.16,
-              epsilon = 0.25,
-              gamma   = 0.01)
+## Create an 'SIR' model with 1600 cattle herds (nodes) and initialize
+## it to run over 4*365 days. Add one infected animal to the first
+## herd to seed the outbreak. Define 'tspan' to record the state of
+## the system at daily time-points. Load scheduled events for the
+## population of nodes with births, deaths and between-node movements
+## of individuals.
+u0 <- u0_SIR()
+u0$I[1] <- 1
+model <- SIR(u0     = u0,
+             tspan  = seq(from = 1, to = 4*365, by = 1),
+             events = events_SIR(),
+             beta   = 0.16,
+             gamma  = 0.01)
 
 ## Display the number of cattle affected by each event type per day.
 plot(events(model))
