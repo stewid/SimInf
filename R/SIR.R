50a3d6f13385a36bfc4096593b0efb729612e38e
Stefan Widgren <stefan.widgren@gmail.com>
2026-04-03 19:05:21 +0200

    Documentation

--- R/SIR.R
+++ R/SIR.R
@@ -195,33 +195,7 @@ SIR <- function(u0,
 ##' details
 ##'
 ##' @export
-##' @examples
-##' ## For reproducibility, call the set.seed() function and specify
-##' ## the number of threads to use. To use all available threads,
-##' ## remove the set_num_threads() call.
-##' set.seed(123)
-##' set_num_threads(1)
-##'
-##' ## Create an 'SIR' model with 1600 cattle herds and initialize
-##' ## it to run over 4*365 days. Add one infected animal to the
-##' ## first herd to seed the outbreak.
-##' u0 <- u0_SIR()
-##' u0$I[1] <- 1
-##' tspan <- seq(from = 1, to = 4*365, by = 1)
-##' model <- SIR(u0     = u0,
-##'              tspan  = tspan,
-##'              events = events_SIR(),
-##'              beta   = 0.16,
-##'              gamma  = 0.01)
-##'
-##' ## Display the number of cattle affected by each event type per day.
-##' plot(events(model))
-##'
-##' ## Run the model to generate a single stochastic trajectory.
-##' result <- run(model)
-##'
-##' ## Summarize the trajectory.
-##' summary(result)
+##' @example man/examples/SIR.R
 events_SIR <- function() {
     utils::data("events_SISe3", package = "SimInf", envir = environment())
     events_SISe3$select[events_SISe3$event == "exit"] <- 4L
@@ -273,38 +247,7 @@ events_SIR <- function() {
 ##' movement and demographic events
 ##'
 ##' @export
-##' @examples
-##' \dontrun{
-##' ## For reproducibility, call the set.seed() function and specify
-##' ## the number of threads to use. To use all available threads,
-##' ## remove the set_num_threads() call.
-##' set.seed(123)
-##' set_num_threads(1)
-##'
-##' ## Create an 'SIR' model with 1600 cattle herds (nodes) and
-##' ## initialize it to run over 4*365 days. Add one infected animal
-##' ## to the first herd to seed the outbreak. Define 'tspan' to record
-##' ## the state of the system at daily time-points. Load scheduled
-##' ## events for the population of nodes with births, deaths and
-##' ## between-node movements of individuals.
-##' u0 <- u0_SIR()
-##' u0$I[1] <- 1
-##' model <- SIR(u0     = u0,
-##'              tspan  = seq(from = 1, to = 4*365, by = 1),
-##'              events = events_SIR(),
-##'              beta   = 0.16,
-##'              gamma  = 0.01)
-##'
-##' ## Run the model to generate a single stochastic trajectory.
-##' result <- run(model)
-##' plot(result)
-##'
-##' ## Plot the trajectory for the first herd.
-##' plot(result, index = 1)
-##'
-##' ## Summarize trajectory
-##' summary(result)
-##' }
+##' @example man/examples/SIR.R
 u0_SIR <- function() {
     utils::data("u0_SISe3", package = "SimInf", envir = environment())
     u0_SISe3$S <- u0_SISe3$S_1 + u0_SISe3$S_2 + u0_SISe3$S_3
