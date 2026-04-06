0e13210de7787f5bf0c18549a06bee4534a7e141
Stefan Widgren <stefan.widgren@gmail.com>
2026-04-06 15:54:24 +0200

    Roxygenize

--- man-roxygen/verbose-param.R
+++ man-roxygen/verbose-param.R
@@ -1,3 +1,2 @@
-##' @param verbose prints diagnostic messages when \code{TRUE}. The
-##'     default is to retrieve the global option \code{verbose} and
-##'     use \code{FALSE} if it is not set.
+##' @param verbose prints diagnostic messages when \code{TRUE}.
+##'     Default is \code{FALSE}.
