d63726a3bfb31ad42bfcd3405d41935989fc67df
Stefan Widgren <stefan.widgren@gmail.com>
2026-04-08 10:41:08 +0200

    docs: update the gamma parameter template

--- man-roxygen/gamma-param.R
+++ man-roxygen/gamma-param.R
@@ -1,5 +1,5 @@
 ##' @param gamma A numeric vector with the recovery rate from infected
-##'     to recovered where each node can have a different gamma
-##'     value. The vector must have length 1 or \code{nrow(u0)}. If
-##'     the vector has length 1, but the model contains more nodes,
-##'     the beta value is repeated in all nodes.
+##'     to recovered. Each node can have a different gamma value.  The
+##'     vector must have length 1 or \code{nrow(u0)}. If the vector
+##'     has length 1 but the model contains more nodes, the gamma
+##'     value is repeated for all nodes.
