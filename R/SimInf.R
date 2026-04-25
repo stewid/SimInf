16c5f6c5444b2e7a4ec01d22662dc3aee83ac6a1
Stefan Widgren <stefan.widgren@gmail.com>
2026-04-25 23:11:40 +0200

    docs: tweak

--- R/SimInf.R
+++ R/SimInf.R
@@ -33,10 +33,11 @@
 ##' SimInf can incorporate data—such as births, deaths, and
 ##' movements—as scheduled events. These events trigger at predefined
 ##' time points and modify the state of subpopulations by randomly
-##' sampling individuals from the affected compartments. This
-##' capability allows simulations to be driven by empirical records or
-##' synthetic scenarios while maintaining the stochastic nature of the
-##' population dynamics.
+##' selecting a specified number of individuals from the affected
+##' compartments. This capability allows simulations to be driven by
+##' empirical records or synthetic scenarios while maintaining the
+##' stochastic nature of population demographics and movement
+##' networks.
 ##'
 ##' The package supports both predefined models (e.g.,
 ##' \code{\link{SIR}}, \code{\link{SIS}}) and custom model
